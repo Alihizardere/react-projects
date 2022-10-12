f54fe5408a1e87dac7e3d3ab4117030790fd5363
Alihizardere <aliduyguhzr@gmail.com>
2022-10-12 04:05:14 +0300

    react projects

---
+++ birthday-reminder/src/data.js
@@ -0,0 +1,31 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {
+      id: 1,
+      name: 'Ayşe Çelik',
+      age: 24,
+      image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    },
+    {
+      id: 1,
+      name: 'Alya Acar ',
+      age: 23,
+      image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+      id: 3,
+      name: 'Ali Hızardere',
+      age: 36,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+      id: 5,
+      name: 'Elif Tunç',
+      age: 17,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+  ]
+  
